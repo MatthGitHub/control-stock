0b2252b631046c62b43734df8e9f66965b143b3a
Claudio Nahuelquin <cnahuelquin@tecnoaccion.com.ar>
2021-09-30 15:23:24 -0300

    swagger

---
+++ src/main/java/com/mercadolibre/stock/config/PomInfo.java
@@ -0,0 +1,38 @@
+package com.mercadolibre.stock.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource(value = "classpath:application.properties")
+@ConfigurationProperties(prefix = "app")
+public class PomInfo {
+    private String name;
+    private String description;
+    private String version;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+}
--- src/main/java/com/mercadolibre/stock/controller/StockController.java
+++ src/main/java/com/mercadolibre/stock/controller/StockController.java
@@ -1,8 +1,11 @@
 package com.mercadolibre.stock.controller;
 
+import com.mercadolibre.stock.model.dto.ItemWrapperDTO;
+import com.mercadolibre.stock.model.dto.PaginationWrapperDTO;
 import com.mercadolibre.stock.model.dto.StockDTO;
 import com.mercadolibre.stock.service.StockService;
 import com.mercadolibre.stock.utils.CheckObjectErrors;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -16,6 +19,7 @@ public class StockController {
     @Autowired
     private StockService stockService;
 
+    @ApiOperation(value = "Cargar stock de productos", response = StockDTO.class)
     @PutMapping("/items/{itemId}")
     public ResponseEntity<?> add(@PathVariable(name = "itemId") String itemId, @Valid @RequestBody StockDTO stockDTO, BindingResult bindingResult) {
         if (bindingResult.hasErrors())
@@ -24,6 +28,7 @@ public class StockController {
         return ResponseEntity.ok(stockService.add(itemId, stockDTO));
     }
 
+    @ApiOperation(value = "Retirar productos de stock", response = StockDTO.class)
     @PutMapping("/items/{itemId}/consume")
     public ResponseEntity<?> consume(@PathVariable(name = "itemId") String itemId, @Valid @RequestBody StockDTO stockDTO, BindingResult bindingResult) {
         if (bindingResult.hasErrors())
@@ -32,6 +37,7 @@ public class StockController {
         return ResponseEntity.ok(stockService.consume(itemId, stockDTO));
     }
 
+    @ApiOperation(value = "Consultar productos segun ubicación y depósito", response = ItemWrapperDTO.class)
     @GetMapping("/items")
     public ResponseEntity<?> getByLocationDeposit(@RequestParam(name = "location") String locationCode,
                                                   @RequestParam(name = "deposit") String depositCode) {
@@ -39,6 +45,7 @@ public class StockController {
         return ResponseEntity.ok(stockService.getByLocationDeposit(locationCode, depositCode));
     }
 
+    @ApiOperation(value = "Busqueda de productos", response = PaginationWrapperDTO.class)
     @GetMapping("/items/search")
     public ResponseEntity<?> search(@RequestParam(name = "deposit") String deposit,
                                     @RequestParam(name = "itemId") String itemId,
